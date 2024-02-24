7a3001e5571d55b90edb3ac8428bd6c7ff1b8b1d
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2024-02-25 00:14:31 +0530

    dynamic content module added for static content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  }
+    path: 'dynamic-content',
+    loadChildren: () =>
+      import('./pages/dynamic-content/dynamic-content.module').then(
+        (m) => m.DynamicContentPageModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  lastUpdated: string = '';
+
   public appPages = [
-    { title: 'About', url: '/about', icon: 'information' },
+    // { title: 'Help', url: '/help', icon: 'help-circle' },
+    { title: 'About', url: '/about', icon: 'information-circle' },
     // { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     // { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
     // { title: 'Archived', url: '/folder/archived', icon: 'archive' },
@@ -14,5 +20,50 @@ export class AppComponent {
     // { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(private router: Router, private http: HttpClient) {}
+  ngOnInit() {
+    this.fetchLastCommitTime();
+  }
+  goToDynamicContentPage() {
+    const pageTitle = 'About me and this app';
+    const htmlContent = `I've slapped this app together in 3-4 days so there's room for improvement. <br><br>
+      If you're a user, suggest what would make it better.<br>
+      If you're a developer, the app is open source. Pull requests are welcome.<br>
+      If you want to volunteer in updating the time sheets, drop me a message at Telegram (@ShakesVision) or WhatsApp. I'll add you as an editor.
+      
+      <h2>Links</h2>
+      
+      GitHub: github.com/ShakesVision <br>
+      Telegram: t.me/ShakesVision <br>
+      WhatsApp: +918956704184 <br>
+      Website: www.sarbakaf.com [Freelance services for websites, apps, e-publishing, writing & more] <br>
+      Blog (English): www.shakeeb.in <br>
+      Blog (Urdu): ur.shakeeb.in <br>
+      `;
+    let navigationExtras: NavigationExtras = {
+      state: { pageTitle, htmlContent },
+    };
+    this.router.navigate(['dynamic-content'], navigationExtras);
+  }
+
+  fetchLastCommitTime() {
+    const repoOwner = 'shakesvision';
+    const repoName = 'prayer';
+    const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/commits`;
+
+    this.http.get(apiUrl).subscribe(
+      (response: any) => {
+        if (response.length > 0) {
+          const lastCommitTime = response[0].commit.author.date;
+          this.lastUpdated = new Date(lastCommitTime).toLocaleString();
+        } else {
+          this.lastUpdated = '';
+        }
+      },
+      (error) => {
+        console.error('Failed to fetch last commit time:', error);
+        this.lastUpdated = '';
+      }
+    );
+  }
 }
