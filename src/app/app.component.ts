837004edeb1ac5a9636ef0a11a58edc43c76716d
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2024-02-25 16:42:49 +0530

    theming fixes, dark theme added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,6 +31,25 @@ export class AppComponent {
   gotopage(url: AppPages['url']) {
     let pageTitle = '';
     let htmlContent = '';
+    let defaultPageTitle = 'About me and this app';
+    let defaultHtmlContent = `<p>I've slapped this app together in 3-4 days so there's room for improvement. </p>
+    <ul>  
+    <li>If you're a user, suggest what would make it better.</li>
+    <li>If you're a developer, the app is open source. Pull requests are welcome.</li>
+    <li>If you want to volunteer to update the time sheets, drop me a message on Telegram or WhatsApp. I'll add you as an editor. </li>
+      </ul>
+    <p>All the timelines are taken from the official WhatsApp group and are updated ASAP in the app as well. As we already have volunteers for each Masjid, we don't necessarily need those many admins for the app (i.e. timesheet used as a backend for this app). We need a few who can update the sheet based on inputs from the WhatsApp group.</p>
+    <p>So the gist is - the WhatsApp group remains the app's main data source. I'll create a short video tutorial for the volunteers on how to update the sheet.</p>
+    <p>Spoiler: It's nothing tricky - just adding the time in 24-hour format. The only thing is not to mess up the whole sheet while doing so. I will work on creating some backup system to avoid so, someday!</p>
+    <p>— Shakeeb Ahmad</p>
+      
+      <h2>Links</h2>
+      <ion-icon slot='start' name='logo-github'></ion-icon> GitHub: <a href="https://github.com/ShakesVision"> @ShakesVision </a> <br>
+      <ion-icon slot='start' name='paper-plane'></ion-icon> Telegram : <a href="https://t.me/ShakesVision"> t.me/ShakesVision </a> <br>
+      <ion-icon slot='start' name='logo-whatsapp'></ion-icon> WhatsApp: <a href="https://wa.me/+918956704184?text=%27Hello,%20saw%20your%20app%20Prayer%20Times%20-%20Kamptee%27"> +918956704184 </a> <br>
+      <ion-icon slot='start' name='globe'></ion-icon> Blog (English) : <a href="https://www.shakeeb.in">shakeeb.in </a> <br>
+      <ion-icon slot='start' name='globe'></ion-icon> Blog (Urdu) : <a href="https://ur.shakeeb.in"> ur.shakeeb.in </a> <br>
+      `;
     switch (url) {
       case 'help':
         pageTitle = 'Help';
@@ -45,25 +64,8 @@ export class AppComponent {
         this.goToDynamicContentPage(pageTitle, htmlContent);
         break;
       case 'about':
-        pageTitle = 'About me and this app';
-        htmlContent = `<p>I've slapped this app together in 3-4 days so there's room for improvement. </p>
-        <ul>  
-        <li>If you're a user, suggest what would make it better.</li>
-        <li>If you're a developer, the app is open source. Pull requests are welcome.</li>
-        <li>If you want to volunteer to update the time sheets, drop me a message on Telegram or WhatsApp. I'll add you as an editor. </li>
-          </ul>
-        <p>All the timelines are taken from the official WhatsApp group and are updated ASAP in the app as well. As we already have volunteers for each Masjid, we don't necessarily need those many admins for the app (i.e. timesheet used as a backend for this app). We need a few who can update the sheet based on inputs from the WhatsApp group.</p>
-        <p>So the gist is - the WhatsApp group remains the app's main data source. I'll create a short video tutorial for the volunteers on how to update the sheet.</p>
-        <p>Spoiler: It's nothing tricky - just adding the time in 24-hour format. The only thing is not to mess up the whole sheet while doing so. I will work on creating some backup system to avoid so, someday!</p>
-        <p>— Shakeeb Ahmad</p>
-          
-          <h2>Links</h2>
-          <ion-icon slot='start' name='logo-github'></ion-icon> GitHub: <a href="https://github.com/ShakesVision"> @ShakesVision </a> <br>
-          <ion-icon slot='start' name='paper-plane'></ion-icon> Telegram : <a href="https://t.me/ShakesVision"> t.me/ShakesVision </a> <br>
-          <ion-icon slot='start' name='logo-whatsapp'></ion-icon> WhatsApp: <a href="https://wa.me/+918956704184?text=%27Hello,%20saw%20your%20app%20Prayer%20Times%20-%20Kamptee%27"> +918956704184 </a> <br>
-          <ion-icon slot='start' name='globe'></ion-icon> Blog (English) : <a href="https://www.shakeeb.in">shakeeb.in </a> <br>
-          <ion-icon slot='start' name='globe'></ion-icon> Blog (Urdu) : <a href="https://ur.shakeeb.in"> ur.shakeeb.in </a> <br>
-          `;
+        pageTitle = defaultPageTitle;
+        htmlContent = defaultHtmlContent;
         this.goToDynamicContentPage(pageTitle, htmlContent);
         break;
       case 'playstore':
@@ -71,6 +73,11 @@ export class AppComponent {
           'https://play.google.com/store/apps/dev?id=8917194327395382081',
           '_blank'
         );
+        break;
+      default:
+        pageTitle = defaultPageTitle;
+        htmlContent = defaultHtmlContent;
+        this.goToDynamicContentPage(pageTitle, htmlContent);
         break;
     }
   }
@@ -94,5 +101,9 @@ export class AppComponent {
         this.lastUpdated = '';
       }
     );
+  }
+
+  changeTheme(event: any) {
+    document.body.classList.toggle('dark');
   }
 }
